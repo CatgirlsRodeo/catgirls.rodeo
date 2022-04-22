64b9cac1c69088092070bba9709c9a02014abdb6
realtradam <github@tradam.dev>
2022-04-22 07:31:24 -0400

    init

---
+++ src/style.css.rb
@@ -0,0 +1,31 @@
+Paggio.css do
+  rule 'body' do
+    height 100.vh
+  end
+
+  rule '.justicar' do
+    top 134.px
+    left 400.px
+    opacity 0.7
+    position 'absolute'
+    width 400.px
+    height 'auto'
+  end
+
+  rule '.opal' do
+    color :dodgerblue
+  end
+
+  rule '.paggio' do
+    color :goldenrod
+  end
+
+  rule '.articles' do
+    color :limegreen
+    max width: 400.px
+  end
+
+  rule 'h1' do
+    font style: :italic
+  end
+end
